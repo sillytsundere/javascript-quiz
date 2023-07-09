352b37b0c0b5c1417d9555c3ac90f349138bd3a3
Paige <paigehcarroll@gmail.com>
2023-07-09 13:06:42 -0500

    added event listener to start button and hid start button upon begining of quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,27 @@
     //data will be stored in local memory
     //button will ask if user wants to restart quiz and take user back to initial page with "start quiz" button
 
-var viewScoresBtn = document.getElementById('#view-scores');
+//This selects the start quiz button
+var startQuizBtn = document.querySelector('#start-quiz');
+//why did getElementById not work?
+
+
+
+//this adds an event listener to the start quiz button to begin the quiz when clicked once
+startQuizBtn.addEventListener('click', function() {
+    console.log('quiz start button works');
+    startQuizBtn.style.visibility = 'hidden'; 
+
+    
+});
+
 
 
 
 //this array holds the possible quiz questions in an array of objects
 //the questions are objects containing key value pairs of the question, choices and correct answer
 //the choices key has a value that is an object containing key value pairs of the answer choices
-var questions = [
+/*var questions = [
     {
         question: 'coding stuff stuff stuff',
         choices: {
@@ -89,3 +102,4 @@ var questions = [
         correctAnswer: 'a',
     },
 ]
+*/
