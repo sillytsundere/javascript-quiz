c655872e1a1ff64c5736df4f1668c544344df785
Paige <paigehcarroll@gmail.com>
2023-07-09 12:02:50 -0500

    added html skeleton for quiz format and added notes and question variable to script document

---
+++ assets/js/script.js
@@ -0,0 +1,91 @@
+//Quiz will start when "start button" is pressed
+//questions will remain hidden until quiz starts
+//upon quiz starting the "start quiz" button will become hidden
+//quiz container will display questions and multiple choice answers
+//user will select an answer and if incorrect time will be taken off the timer
+//upon selecting an answer user will be presented with incorrect or correct message and a "next question" button
+//after all questions are answered quiz will say "well done"
+    //quiz will display final score
+    //quiz will ask for initials to save final score
+    //data will be stored in local memory
+    //button will ask if user wants to restart quiz and take user back to initial page with "start quiz" button
+
+var viewScoresBtn = document.getElementById('#view-scores');
+
+
+
+//this array holds the possible quiz questions in an array of objects
+//the questions are objects containing key value pairs of the question, choices and correct answer
+//the choices key has a value that is an object containing key value pairs of the answer choices
+var questions = [
+    {
+        question: 'coding stuff stuff stuff',
+        choices: {
+            a: 'choice a',
+            b: 'choice b',
+            c: 'choice c',
+            d: 'choice d',
+        },
+        correctAnswer: 'a',
+    },
+    {
+        question: 'coding stuff stuff stuff',
+        choices: {
+            a: 'choice a',
+            b: 'choice b',
+            c: 'choice c',
+            d: 'choice d',
+        },
+        correctAnswer: 'a',
+    },
+    {
+        question: 'coding stuff stuff stuff',
+        choices: {
+            a: 'choice a',
+            b: 'choice b',
+            c: 'choice c',
+            d: 'choice d',
+        },
+        correctAnswer: 'a',
+    },
+    {
+        question: 'coding stuff stuff stuff',
+        choices: {
+            a: 'choice a',
+            b: 'choice b',
+            c: 'choice c',
+            d: 'choice d',
+        },
+        correctAnswer: 'a',
+    },
+    {
+        question: 'coding stuff stuff stuff',
+        choices: {
+            a: 'choice a',
+            b: 'choice b',
+            c: 'choice c',
+            d: 'choice d',
+        },
+        correctAnswer: 'a',
+    },
+    {
+        question: 'coding stuff stuff stuff',
+        choices: {
+            a: 'choice a',
+            b: 'choice b',
+            c: 'choice c',
+            d: 'choice d',
+        },
+        correctAnswer: 'a',
+    },
+    {
+        question: 'coding stuff stuff stuff',
+        choices: {
+            a: 'choice a',
+            b: 'choice b',
+            c: 'choice c',
+            d: 'choice d',
+        },
+        correctAnswer: 'a',
+    },
+]
